013ee84b360e8b83d2419831bcc69c0df7c17a46
viswavichuvenkat <wcclife2124@gmail.com>
2024-09-02 05:59:21 +0530

    updated notification

---
+++ Viswa-notification-updated/payment_notification/src/main/java/com/billing/payment_notification/DTO/BillingDTO.java
@@ -0,0 +1,37 @@
+package com.billing.payment_notification.DTO;
+
+public class BillingDTO {
+
+    private Long id;
+    private Long customerId;
+    private String paymentStatus;
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(Long customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    
+}
+
+
