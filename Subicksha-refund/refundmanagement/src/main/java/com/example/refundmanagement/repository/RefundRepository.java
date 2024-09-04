75bf968b93cb5d3a8c8b26d510cc6c2204277aa6
viswavichuvenkat <wcclife2124@gmail.com>
2024-09-04 12:54:26 +0530

    Delete Subicksha-refund directory

--- Subicksha-refund/refundmanagement/src/main/java/com/example/refundmanagement/repository/RefundRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.refundmanagement.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.refundmanagement.entity.Refund;
-
-public interface RefundRepository extends JpaRepository<Refund,Long>{
-
-}
