05b957bbf49168d8c9702e6dff5cf0fc38c8650e
viswavichuvenkat <wcclife2124@gmail.com>
2024-09-04 12:55:25 +0530

    Delete sai prabha S-customer directory

--- sai prabha S-customer/src/main/java/com/dummy/dummybawa/service/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.dummy.dummybawa.service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
