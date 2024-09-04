f069f0d7abc76a4b88335b8beeee552dc5285490
viswavichuvenkat <wcclife2124@gmail.com>
2024-09-04 12:55:16 +0530

    Delete project sai directory

--- project sai/customer/src/main/java/com/soi/customer/service/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.soi.customer.service;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
