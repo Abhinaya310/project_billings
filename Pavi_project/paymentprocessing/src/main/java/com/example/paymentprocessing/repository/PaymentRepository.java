48c09d48448b53dcc28fccdc9b000bc208cd0f2b
viswavichuvenkat <wcclife2124@gmail.com>
2024-09-04 12:54:08 +0530

    Delete Pavi_project directory

--- Pavi_project/paymentprocessing/src/main/java/com/example/paymentprocessing/repository/PaymentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.paymentprocessing.repository;
-
-import com.example.paymentprocessing.model.Payment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PaymentRepository extends JpaRepository<Payment, Long> {
-}
